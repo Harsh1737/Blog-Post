2b80bfdec0baf7ff8a9c5197058d12c191492d86
Harsh <harshbit10@gmail.com>
2024-09-08 16:20:46 +0530

    Standard components

---
+++ Blog app/src/components/Container/Container.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const container = ({ childern }) => {
+  return <div className="w-full max-w-7xl mx-auto px-4">{childern} </div>;
+  
+  // 1 line return doesnt require ( )
+};
+
+export default container;
--- Blog app/src/store/store.js
+++ Blog app/src/store/store.js
@@ -1,9 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import authReducer from './authSlice';
 const store = configureStore({
     reducer: {
-        
+        auth: authReducer,
     }
 })
 
 export default store;
+
+
+    // {
+    //     "auth": {
+    //       "status": false,
+    //       "userData": null
+    //     }
+    //   }
+  
