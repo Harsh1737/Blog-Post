c5b4a899e4f0808e8516910f2a0cc96bf6c9559d
Harsh <harshbit10@gmail.com>
2024-09-08 10:10:41 +0530

    Store access and Stage 1 testing

--- Blog app/src/App.jsx
+++ Blog app/src/App.jsx
@@ -1,12 +1,42 @@
-
-
+import { useState , useEffect } from "react"
+import { useDispatch } from 'react-redux'
+import './App.css'
+import authService from './appwrite/auth'
+import { login, logout} from './store/authSlice'
+import { Header, Footer} from './components'
 function App() {
 
-  return (
-    <>
-      <div> A blog app with appwrite</div>
-    </>
-  )
+  const [ loading, setLoading ] = useState( true)
+  // beacause useEffect do some works on mounting
+  // will make it false inside useEffect
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then( (userData) => {
+      if ( userData ){
+        dispatch(login({userData}))
+      } else{
+        dispatch(logout({userData}))
+      }
+    })
+    // .catch()
+    .finally( () => setLoading(false))
+  },[])
+
+  return !loading ? (
+    <div 
+    className="min-h-screen flex flex-wrap content-between bg-gray-500">
+    <div className="w-full block">
+      <Header />
+      <main>
+        {/* < Outlet /> */}
+      </main>
+      <Footer />
+    </div>
+    </div>
+  ) : null
 }
 
 export default App
